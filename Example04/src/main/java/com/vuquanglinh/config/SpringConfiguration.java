7c9937c6b12984cf0900c0ac82179d611cb5f842
DCL IT <DCL IT@DESKTOP-BVIF3GT.mshome.net>
2020-10-03 03:57:24 -0700

    Example04

---
+++ Example04/src/main/java/com/vuquanglinh/config/SpringConfiguration.java
@@ -0,0 +1,25 @@
+package com.vuquanglinh.config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configurable
+@EnableWebMvc
+@ComponentScan(basePackages="com.vuquanglinh.controller")
+public class SpringConfiguration {
+	@Bean(name="jspViewResolver")
+	public ViewResolver viewResolver()
+	{
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("WEB-INF/views/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+		
+	}
+}
