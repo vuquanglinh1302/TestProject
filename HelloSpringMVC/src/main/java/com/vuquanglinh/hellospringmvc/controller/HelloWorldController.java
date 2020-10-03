2a66476549523f8fd739a47378e02b8a76c12f99
DCL IT <DCL IT@DESKTOP-BVIF3GT.mshome.net>
2020-10-03 03:50:54 -0700

    Fisrt Commit

---
+++ HelloSpringMVC/src/main/java/com/vuquanglinh/hellospringmvc/controller/HelloWorldController.java
@@ -0,0 +1,24 @@
+package com.vuquanglinh.hellospringmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloWorldController 
+{
+	String message = "Welcome to Spring MVC!";
+	@RequestMapping("/hello")
+	public ModelAndView showMessage(
+		@RequestParam(value = "name", required = false, defaultValue = "world") String name)
+		{
+			System.out.println("in controller");
+			ModelAndView mv = new ModelAndView("helloworld");
+			mv.addObject("message", message);
+			mv.addObject("name", name);
+			return mv;
+		}
+	
+	
+}
